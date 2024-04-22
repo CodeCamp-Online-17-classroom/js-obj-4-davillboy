2f0919bf86f9b77745a60d94ba6877d1b4834613
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 14:53:04 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // write code here
+
+ <script>
+        let username = prompt("Enter your username:");
+        let email = prompt("Enter your email:");
+        let password = prompt("Enter your password:");
+
+        
+        let user = {
+        username: username,
+        email: email,
+        password: password
+        };
+
+        console.log(user); 
+
+  </script>
